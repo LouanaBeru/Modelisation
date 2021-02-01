ba60c727541cdb0d51dc7a51c28cd97ab7f37ed3
LouanaBeru <78320911+LouanaBeru@users.noreply.github.com>
2021-02-01 14:33:08 +0100

    dérivatives1d_modifie renommé en projet1

--- projet1.py
+++ projet1.py
@@ -1,120 +1,82 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 27 08:57:18 2021
-
-@author: Chapati
-"""
-
-#numpy.gradient(f,varargs, axis, edge_order) -> donne le gradient d'une fonction
-
-#Se placer sur un intervalle et définir un nombre de points, créer le vecteur x, qui a pour coordonnées les différents points
-# ensuite, demander à l'utilisateur de rentrer une fonction f en fonction de x
-#créer un vecteur f qui a pour coordonnées f appliquée aux x[i]
-# => on aura les valeurs de f sur tous nos points en discret
-
-
-# calculer le gradient, div, laplacien par le meme principe
-
-# affichage : relier les points
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from matplotlib import cm
+from array import array
 
-L = 5 #longueur de l'axe X, on est sur ]0;5[
-NX = 100 #Nombre de points sur l'axe X
-NT = 100
-dx = L/NX # distance entre les points
-x = [] #Les différents points sur l'axe X
-f = [] #les differentes valeurs de f pour chaque x
-lapf = [] #laplacien
-gradf = [] #gradient
-divf = [] #divergence
-n= random.randint(3, 200) #génère un int entre 3 et 200 compris
-h= 1/(n-1)
-normErrGrad = []
-normErrLap = []
 
-##FONCTIONS UTILES##
+def tri(list):
+    list.sort()
+    return list
+
 def norm2(list):
     norm2 = 0
     for i in range(len(list)):
         norm2 = norm2 + list[i]**2
     return np.sqrt(norm2)
 
-##CREATION DES POINTS SUR L'AXE X##
-x = np.arange(0, L, dx) #On crée tous les points entre 0 et 5 espacés régulièrement
+##VARIABLES
+L1=[]
+L2=[]
+N=[]
+NL1=[]
+NL2=[]
 
-##CREATION DE LA FONCTION##
-def u(x):
-    f=[]
-    for i in range (0, NX):
-        f = f + [(x[i])**4] #On construit la liste des différentes valeurs prises par la fonction, ici x**4
-    return f
+# génère un entier au hasard entre 3 et 200 et on met les différentes valeurs prises dans une liste
+N=random.choices(range(3, 201), k=20) 
+N=tri(N) # on tri par ordre croissant ( pour l'affichage)
 
-f=u(x)
 
-##CALCUL DU GRADIENT##
-for i in range(len(x)-2):
-    gradf.append( (f[i]+f[i+2])/(2*dx) ) #marche que jusqu'à 97, car on fait x[i+2] et x a des coordonnées de 0 à 99
-for i in range(len(x)-2, len(x)): #il faut créer les deux dernières coordonnées du vecteur grad
-    gradf.append( 2*gradf[i-1]-gradf[i-2] ) #rajoute une coordonnées à la liste
+for i in range(20) : #on le fait 20 fois
+    # NUMERICAL PARAMETERS
+    NX = N[i] #ieme nombre de points du maillage ( que je noterai NXi dans les légendes)
+    L=2*3.141592   #2*math.pi
+    dx = L/(NX-1)  #Grid step (space)
+
+    # Initialisation
+    x = np.linspace(0.0,L,NX) #x = [x_0, ..., x_NXi] -> la liste des points du maillage
+
+    T = np.sin(x)
+    Txe = np.cos(x)
+    Txxe = -np.sin(x)
+
+    Tx = np.zeros((NX))
+    Txx = np.zeros((NX))
+
+    #discretization of the second order derivative (Laplacian)
+    for j in range (1, NX-1):
+        Tx[j] = (T[j+1]-T[j-1])/(dx*2)
+        Txx[j] = (T[j-1]-2*T[j]+T[j+1])/(dx**2)
+        
+    #Tx and Txx on boundaries
+    # use extrapolation in order to have (T,x),xx=0
+    #(T,x),xx= (Tx0 -2*Tx1+Tx2) =0
+    Tx[0] = 2*Tx[1]-Tx[2]
+    Tx[NX-1] = 2*Tx[NX-2]-Tx[NX-3]
+    Txx[0] = 2*Txx[1]-Txx[2]
+    Txx[NX-1] = 2*Txx[NX-2]-Txx[NX-3]
+
+    L1= abs(Tx-Txe) #liste de liste des erreurs en chaque point du maillage pour la première dérivée
+    L2= abs(Txx-Txxe) #liste de liste des erreurs pour la deuxième dérivée
+    NL1 = NL1 + [norm2(L1)]
+    NL2 = NL2 + [norm2(L2)]
+
+plt.figure(1) #pour afficher
+plt.xlabel(u'$NX$', fontsize=26)
+plt.ylabel(u'$Erreur$', fontsize=26, rotation=90)
+plt.plot(N,np.log10(NL1),label='L1',color=(1,0,0,1)) #On trace l'erreur (abs(Tx-Txe) pour tous les points xi pour la couleur, il fallait que la couleur soit moins intense quand il y a moins de points ( la couleur s'exprime en RGBA, A étant la transparence) et c'est un pourcentage ( donc un nbr entre 0 et 1)
+plt.title(u'Evolution de l\' erreur pour la première dérivée')   
     
-def ux(x):
-    fx=[]
-    for i in range (0, NX):
-        fx = fx + [4*(x[i])**3] #On construit la liste des différentes valeurs prises par la fonction, ici x**4
-    return fx
-
-fx=ux(x)
-
-##CALCUL DE LA DIVERGENCE##
-for i in range(0, NX-2):
-    divf.append( gradf[i] )
-for i in range(len(x)-2, len(x)):
-    divf.append( 2*divf[i-1]-divf[i-2] )
-
-##CALCUL DU LAPLACIEN##
-for i in range (0, NX-2):
-    lapf.append( (gradf[i]+gradf[i+2])/(2*dx))
-for i in range(len(x)-2, len(x)):
-    lapf.append( (2*lapf[i-1]-lapf[i-2]) )
-
-def uxx(x):
-    fxx=[]
-    for i in range (0, NX):
-        fxx = fxx + [12*(x[i])**2] #On construit la liste des différentes valeurs prises par la fonction, ici x**4
-    return fxx
-
-fxx=uxx(x)
-
-##CALCUL DES ERREURS##
-errGrad= abs(np.array(gradf)-np.array(fx)) #liste de liste des erreurs en chaque point du maillage pour la première dérivée
-errLap= abs(np.array(lapf)-np.array(fxx)) #liste de liste des erreurs pour la deuxième dérivée
-normErrGrad = h * norm2(errGrad)
-normErrLap = h*norm2(errLap)
-
-
-plt.figure(1)
-plt.plot(x,f,label='courbe de f',color='black')
-plt.plot(x,gradf,label='courbe de grad(f)',color='blue')
-plt.plot(x,lapf,label='courbe de lapl(f)',color='green')
-plt.legend((('f(x)'), ('grad(f)'), ('div(f)')))
-plt.title('Projet 1')
-plt.show()
-
-plt.figure(2)
-plt.plot(x,f,label='courbe de f',color='black')
-plt.plot(x,gradf,label='courbe de grad(f)',color='blue')
-plt.plot(x,lapf,label='courbe de lapl(f)',color='green')
-plt.legend((('f(x)'), ('grad(f)'), ('div(f)')))
-plt.title('Projet 1')
+plt.figure(2) #pour afficher
+plt.xlabel(u'$NX$', fontsize=26)
+plt.ylabel(u'$Erreur$', fontsize=26, rotation=90)
+plt.plot(N,np.log10(NL2),label='L2',color=(0,0,1,1)) 
+plt.title(u'Evolution de l\' erreur pour la deuxième dérivée')
 plt.show()
 
 
-
-
-
-
-
+#ameliorer ce code en ajoutant les fonctionalites suivantes:
+# parameterer NX et etudier l'evolution de l'erreur L1 pour 20 valeurs de NX de 3 a 200
+# tracer les courbes de l'erreur en fonction de NX en echelle log10
 
 
